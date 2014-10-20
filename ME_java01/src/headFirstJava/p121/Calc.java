3e4fedc062dd0ae0f89741611b6aee0840d160ad
tiggerk <tiggerhyun@gmail.com>
2014-10-21 00:51:12 +0900

    .

---
+++ ME_java01/src/headFirstJava/p121/Calc.java
@@ -0,0 +1,10 @@
+package headFirstJava.p121;
+
+public class Calc {
+	int calcArea (int height, int width) {
+		return height * width;
+	}
+	int a = calcArea(7, 12);
+	short c = 7;
+// 모르겠당 ㅡㅡ
+}
---
+++ ME_java01/src/headFirstJava/p122/Clock.java
@@ -0,0 +1,13 @@
+package headFirstJava.p122;
+
+class Clock {
+	String time;
+	
+	void setTime(String t) {
+		time = t;
+	}
+	
+	String getTime() {
+		return time;
+	}
+}
