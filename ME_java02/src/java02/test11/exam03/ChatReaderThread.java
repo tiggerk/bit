775c7e3906c5c04bdd1ab471ca849666d65a73ed
tiggerk <tiggerhyun@gmail.com>
2014-11-06 08:02:07 +0900

    .

---
+++ ME_java02/src/java02/test11/exam03/ChatReaderThread.java
@@ -0,0 +1,25 @@
+package java02.test11.exam03;
+
+import java.util.Scanner;
+
+public class ChatReaderThread extends Thread {
+	Scanner in;
+
+	public ChatReaderThread(Scanner in) {
+		this.in = in;
+	}
+
+	@Override
+	public void run() {
+		try {
+			String message = null;
+			while (true) {
+				message = in.nextLine();
+				System.out.println("\n==> " + message);
+			}
+		} catch (Exception e) {
+			System.out.println("데이터 수신 중 오류 발생!");
+
+		}
+	}
+}
