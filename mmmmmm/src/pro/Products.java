1a5db8c2413a2afd51dcc5f8b55ce1966cf1a533
tiggerk <tiggerhyun@gmail.com>
2014-11-11 00:16:36 +0900

    .

--- mmmmmm/src/pro/Products.java
+++ mmmmmm/src/pro/Products.java
@@ -6,123 +6,131 @@ import java.sql.Statement;
 import java.util.Scanner;
 
 public class Products {
-  Connection con = null;
-  Scanner scanner;
-  Statement stmt = null;
-  
-  public void init() {
-    try {
-      Class.forName("com.mysql.jdbc.Driver");
+	Connection con = null;
+	Scanner scanner;
+	Statement stmt = null;
 
-      con = DriverManager.getConnection(
-          "jdbc:mysql://localhost:3306/studydb", 
-          "study",
-          "study");
-      System.out.println("DBMS에 연결됨");
+	public String promptCommand(Scanner scanner) {
+		System.out.print("명령 > ");
 
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-  } // end of init
-  
-  public String promptCommand(Scanner scanner) {
-    System.out.print("명령 > ");
+		String scan = scanner.nextLine();
+		return scan;
+	}
+	
+	/*
+	private String[] promptCommand() {
+	    System.out.print("명령>");
+	    String[] token = scanner.nextLine().split(" ");
+	    return token;
+	  }
+	  */
 
-    String scan = scanner.nextLine();
-    return scan;
-  }
-  
-  public void menu() {
-    scanner = new Scanner(System.in);
+	public void init() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
 
-    loop:
-      while (true) {
-        try {
-          String scan = promptCommand(scanner);
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/studydb", 
+					"study",
+					"study");
+			System.out.println("DBMS에 연결됨");
 
-          switch (scan) {
-          case "add":
-            System.out.println("add입니다.");
-            add();
-            break;
-          case "list":
-            System.out.println("list입니다.");
-            break;
-          case "update":
-            System.out.println("update입니다.");
-            break;
-          case "delete":
-            System.out.println("delete입니다.");
-            delete();
-            break;
-          case "exit":
-            break loop;
-          default:
-            System.out.println("잘못누름");
-            break;
-          }
-        } catch (Exception e) {}
-      }
-  } // end of menu
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	} // end of init
 
-  private void delete() {
-    String scan;
+	public void menu() {
+		scanner = new Scanner(System.in);
 
-    try {
-      System.out.println(아이폰6플러스 + "를 삭제하시겠습니까?(y/n)");
-      scan = scanner.nextLine();
+		loop:
+			while (true) {
+				try {
+					String scan = promptCommand(scanner);
 
-      if (scan.equalsIgnoreCase("y")) {
-        stmt = con.createStatement();
-        stmt.executeUpdate("DELETE FROM PRODUCTS" +
-            " WHERE PNO IN("+아이폰6플러스번호+")");
-      } else {
-        System.out.println("저장 취소하였습니다.");
-      }
+					switch (scan) {
+					case "add":
+						System.out.println("add입니다.");
+						add();
+						break;
+					case "list":
+						System.out.println("list입니다.");
+						break;
+					case "update":
+						System.out.println("update입니다.");
+						break;
+					case "delete":
+						System.out.println("delete입니다.");
+						delete();
+						break;
+					case "exit":
+						break loop;
+					default:
+						System.out.println("잘못누름");
+						break;
+					}
+				} catch (Exception e) {}
+			}
+	} // end of menu
 
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
+	private void delete() {
+		String scan;
 
-  }
+		try {
+			System.out.println(아이폰6플러스 + "를 삭제하시겠습니까?(y/n)");
+			scan = scanner.nextLine();
 
-  private void add() {
-    String pno;
-    int qty;
-    int mkno;
-    String scan;
-    
-    try {
-      System.out.println("제품명 :");
-      pno = scanner.nextLine();
-      
-      System.out.println("수량 :");
-      qty = scanner.nextInt();
-      
-      System.out.println("제조사 :");
-      mkno = scanner.nextInt();
-            
-      System.out.println("저장하시겠습니까?(y/n)");
-      scan = scanner.nextLine();
-      
-      if (scan.equalsIgnoreCase("y")) {
-        stmt = con.createStatement();
-        stmt.executeUpdate("INSERT INTO PRODUCTS(PNO,QTY,MKNO)" +
-            " VALUES('"+pno+"',"+qty+","+mkno+")");
-      } else {
-        System.out.println("저장 취소하였습니다.");
-      }
-      
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-  }
+			if (scan.equalsIgnoreCase("y")) {
+				stmt = con.createStatement();
+				stmt.executeUpdate("DELETE FROM PRODUCTS" +
+						" WHERE PNO IN("+아이폰6플러스번호+")");
+			} else {
+				System.out.println("저장 취소하였습니다.");
+			}
 
-  public void destroy() {
-    try {con.close();} catch (Exception ex) {}
-    System.out.println("DBMS와 연결 끊음");
-    try {scanner.close();} catch (Exception ex) {}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-  } // end of destroy
+	}
+
+	private void add() {
+		String pno;
+		int qty;
+		int mkno;
+		String scan;
+
+		try {
+			System.out.println("제품명 :");
+			pno = scanner.nextLine();
+
+			System.out.println("수량 :");
+			qty = scanner.nextInt();
+
+			System.out.println("제조사 :");
+			mkno = scanner.nextInt();
+
+			System.out.println("저장하시겠습니까?(y/n)");
+			scan = scanner.nextLine();
+
+			if (scan.equalsIgnoreCase("y")) {
+				stmt = con.createStatement();
+				stmt.executeUpdate("INSERT INTO PRODUCTS(PNO,QTY,MKNO)" +
+						" VALUES('"+pno+"',"+qty+","+mkno+")");
+			} else {
+				System.out.println("저장 취소하였습니다.");
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void destroy() {
+		try {con.close();} catch (Exception ex) {}
+		System.out.println("DBMS와 연결 끊음");
+		try {scanner.close();} catch (Exception ex) {}
+
+	} // end of destroy
 
 }
